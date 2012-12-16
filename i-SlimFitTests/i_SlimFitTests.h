bf7eb4fe3fabafc50e6ff6c30fd851ce5a94a7e6
Sornsiri <sornsiri@Sornsiris-MacBook-Air.local>
2012-12-16 15:25:13 +0700

    Initial Commit

---
+++ i-SlimFitTests/i_SlimFitTests.h
@@ -0,0 +1,13 @@
+//
+//  i_SlimFitTests.h
+//  i-SlimFitTests
+//
+//  Created by Sornsiri on 12/16/12.
+//  Copyright (c) 2012 Sornsiri. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface i_SlimFitTests : SenTestCase
+
+@end
---
+++ i-SlimFitTests/i_SlimFitTests.m
@@ -0,0 +1,32 @@
+//
+//  i_SlimFitTests.m
+//  i-SlimFitTests
+//
+//  Created by Sornsiri on 12/16/12.
+//  Copyright (c) 2012 Sornsiri. All rights reserved.
+//
+
+#import "i_SlimFitTests.h"
+
+@implementation i_SlimFitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in i-SlimFitTests");
+}
+
+@end
